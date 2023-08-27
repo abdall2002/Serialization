75591f87843691396313dac3d46ad936055908cd
unknown <abdallahaliahmed517@gmail.com>
2023-08-27 15:19:36 +0300

    SSS

--- CAHttpClientJson/Program.cs
+++ CAHttpClientJson/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using CAHttpClientJson;
+using System.Net.Http;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+private readonly static HttpClient httpClient = new HttpClient();
+static async Task Main(string[] args)
+{
+    var todoesJsonContent = await httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+
+    var todoes = JsonSerializer.Deserialize<List<Todo>>(todoesJsonContent
+        , new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+    foreach (var item in todoes)
+        Console.WriteLine(item);
+}
+
+
+
---
+++ CAHttpClientJson/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAHttpClientJson
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"\n [{Id} - UserId: {UserId}] -  {Title} {(Completed ? "completed" : "not completed")}";
+        }
+    }
+}
