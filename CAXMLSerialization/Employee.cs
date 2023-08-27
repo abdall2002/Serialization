2644b249eebfcdc17f74d189dacafe3d10f82f9b
unknown <abdallahaliahmed517@gmail.com>
2023-08-27 13:50:36 +0300

    SSS

---
+++ CAXMLSerialization/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAXMLSerialization
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; } 
+        public List<string> Benefits { get; set; } = new List<string>();
+
+    }
+}
---
+++ CAXMLSerialization/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+using System.Xml;
+using CAXMLSerialization;
+
+var e1 = new Employee
+{
+   Id = 1001,
+   Fname = "Issam",
+   Lname = "A.",
+   Benefits = { "Pension", "Health Insurance" }
+};
+
+var xmlContent = SerializeToXmlString(e1);
+Console.WriteLine(xmlContent);
+static string SerializeToXmlString(Employee e1)
+{
+    var result = "";
+    var xmlSerializer = new XmlSerializer(e1.GetType());
+    using (var sw = new StringWriter())
+    {
+        using (var writer = XmlWriter.Create(sw, new XmlWriterSettings { Indent = true }))
+        {
+            xmlSerializer.Serialize(writer, e1);
+            result = sw.ToString();
+        }
+    }
+    return result;
+}
